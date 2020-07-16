7c70573b05538abe2cb029e04fc064c05d0816a4
Jonas Kaltenbach <jonas.kaltenbach@inovex.de>
2020-07-16 11:37:58 +0200

    feat: export formatNumbers pipe

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
 export { NgxLocalizedNumbers } from "./src/localized-numbers.module";
 export { LocalizationFormatCurrencyPipe } from "./src/format-currency.pipe";
+export { LocalizationFormatNumberPipe } from "./src/format-number.pipe";
 export { NgxLocalizedNumbersService } from "./src/localized-numbers.service";
