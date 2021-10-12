9288ecad03bfbe7f8d8c1bb8dcb6664a79c3d4f8
mbenzenhoefer <mb@mb-consult.io>
2021-10-12 08:02:43 +0200

    update for ng10

--- src/localized-numbers.module.ts
+++ src/localized-numbers.module.ts
@@ -4,19 +4,19 @@ import { NgxLocalizedNumbersService } from './localized-numbers.service';
 import { LocalizationFormatCurrencyPipe } from './format-currency.pipe';
 import { LocalizationFormatNumberPipe } from './format-number.pipe';
 
-let NUMBERS_DECLARATIONS: any = [
+let PIPE_DECLARATIONS: any = [
   LocalizationFormatCurrencyPipe,
   LocalizationFormatNumberPipe
 ];
 
 @NgModule({
   imports: [],
-  exports: [NUMBERS_DECLARATIONS],
-  declarations: [NUMBERS_DECLARATIONS],
+  exports: [PIPE_DECLARATIONS],
+  declarations: [PIPE_DECLARATIONS],
   providers: []
 })
 export class NgxLocalizedNumbers {
-  static forRoot(): ModuleWithProviders {
+  static forRoot(): ModuleWithProviders<NgxLocalizedNumbers> {
     return {
       ngModule: NgxLocalizedNumbers,
       providers: [NgxLocalizedNumbersService]
